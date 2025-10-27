19b45def996d03e57a6878633ce797b3232f8625
chandraa-ads <chandraaads1@gmail.com>
2025-10-27 15:04:31 +0530

    . env file

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,17 +1,19 @@
-// ormconfig.ts
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
+
 dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  entities: ['src/**/*.entity{.ts,.js}'],
-  migrations: ['src/migrations/*{.ts,.js}'],
-  synchronize: false,
+  url:
+    process.env.DATABASE_URL ||
+    `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  ssl: {
+    rejectUnauthorized: false, // required for Supabase SSL connections
+  },
+  entities: ['dist/**/*.entity{.ts,.js}', 'src/**/*.entity{.ts,.js}'],
+  migrations: ['dist/migrations/*{.ts,.js}', 'src/migrations/*{.ts,.js}'],
+  synchronize: false, // always keep this false in production
+  logging: true,
 });
